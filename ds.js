6cfbe8dbc47beff637c26c6c9fbb6cb076d620f7
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-05 11:33:38 -0500

    Initial skeleton commit.

---
+++ ds.js
@@ -0,0 +1,26 @@
+/**
+ * Implements hook_field_formatter_view().
+ */
+function ds_field_formatter_view(entity_type, entity, field, instance, langcode, items, display) {
+  try {
+    
+    /*dpm(entity_type);
+    dpm(entity);
+    dpm(field);
+    dpm(instance);
+    dpm(langcode);
+    dpm(items);
+    dpm(display);*/
+
+    return '';
+    
+    // Iterate over each item, and place a widget onto the render array.
+    var content = {};
+    $.each(items, function(delta, item) {
+        content[delta] = { markup: '' }; }
+    });
+    return content;
+  }
+  catch (error) { console.log('ds_field_formatter_view - ' + error); }
+}
+
